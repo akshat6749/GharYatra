a29dbb8f0224e00dbec8e0f1c752c793a7e629df
Akshat Jaiswal <akshatjaiswal872@gmail.com>
2025-07-09 00:59:53 +0530

    saf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gharyatri',
-        'USER': 'root',
-        'PASSWORD': 'akshat',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
