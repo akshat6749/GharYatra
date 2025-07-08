c9c00768095a154675fea5190f241d35315adc98
Akshat Jaiswal <akshatjaiswal872@gmail.com>
2025-07-09 00:46:56 +0530

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gharyatri',
+        'USER': 'root',
+        'PASSWORD': 'akshat',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
