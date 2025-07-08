aed54aeb55ba04e3e23a0489499085ff32686bb6
Akshat Jaiswal <akshatjaiswal872@gmail.com>
2025-07-09 00:54:16 +0530

    abxc

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-!i3fr%b#3c2awd2y&_tmf0ps-9kehm=maxjus$nyih=qq(y^jp
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0','gharyatra.onrender.com']
 
 
 # Application definition
